b253d78643a4ab75616b2bea1fcf07b36db796ca
ozavala <oszavala@gmail.com>
2025-04-16 20:32:59 -0500

    Refactor permission checks to use enum cases directly; update views and controllers for consistency. Add RoleEnum for role management.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,6 +15,6 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create(['name' => RoleEnum::ADMIN]);
-Role::create(['name' => RoleEnum::USER]);
+        Role::create(['name' => RoleEnum::USER]);
     }
 }
